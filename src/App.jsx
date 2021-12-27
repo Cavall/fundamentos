784ccd7abd3df77a272acb21e9c8fa94181ba25d
Cavall <antoniodonizethe@hotmail.com>
2021-12-27 19:04:00 -0300

    MUDANDO DO TRADICIONAL PARA ARROW FUNCTION

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import PrimeiroComParametros from "./components/basicos/PrimeiroComParametros";
-import Fragmento from "./components/basicos/Fragmentos";
 import Fragmentos from "./components/basicos/Fragmentos";
 
-export default function App(props) {
-  return (
-    <div id="app">
-      <h1>FUNDAMENTOS REACT COM COMPONENTE APP</h1>
-      <Fragmentos></Fragmentos>
+export default () => (
+  <div id="app">
+    <h1>FUNDAMENTOS REACT COM COMPONENTE APP E ARROW FUNCTION</h1>
+    <Fragmentos />
 
-      <PrimeiroComParametros
-        titulo="Situação do Aluno"
-        aluno="Antonio "
-        nota={9.4}
-      />
+    <PrimeiroComParametros
+      titulo="Situação do Aluno"
+      aluno="Antonio "
+      nota={9.4}
+    />
 
-      <Primeiro></Primeiro>
-    </div>
-  );
-}
+    <Primeiro />
+  </div>
+);
