78e0884e18efe0b09553c606954d31dadd47a8f7
Cavall <antoniodonizethe@hotmail.com>
2021-12-26 22:27:00 -0300

    Meu primeiro componente no react com jsx e import React

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,12 @@
+import React from "react";
+
 export default function Primeiro() {
-  return "MEU PRIMEIRO COMPONENTE REACT SEM JSX";
+  const msg = "Seja meu Amigo";
+  return (
+    <div>
+      <h2> Meu Primeiro Componente com JSX</h2>
+      <p>Tivemos que importar o React, com R maisculo, neste arquivo .js</p>
+      <p> {msg} </p>
+    </div>
+  );
 }
