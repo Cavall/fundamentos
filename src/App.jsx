adb3bd64409093a52af5d366856f5783bbdc88e2
Cavall <antoniodonizethe@hotmail.com>
2022-01-02 15:36:58 -0300

    Incluso card simples sem nada de css inclusive

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Primeiro from "./components/basicos/Primeiro";
 import PrimeiroComParametros from "./components/basicos/PrimeiroComParametros";
 import Fragmentos from "./components/basicos/Fragmentos";
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default () => (
   <div id="app">
-    <h1>FUNDAMENTOS REACT COM COMPONENTE APP E ARROW FUNCTION</h1>
+    <h1>FUNDAMENTOS REACT</h1>
+
+    <Card></Card>
     <Aleatorio min={1} max={100} />
     <Fragmentos />
 
